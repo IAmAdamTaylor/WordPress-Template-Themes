71fc4241aa18ac4cdceaabd41a3e8a3e7d7c06a9
Adam Taylor <AdamTaylor@core-marketing.co.uk>
2018-06-08 12:11:58 +0100

    Add likely to be overridden templates to child

---
+++ basetheme-child/page.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ */
+
+get_header();
+
+?>
+
+<?php while ( have_posts() ): ?>
+	<?php
+		the_post();
+	?>
+
+	<div class="container">
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+					
+			<h2 class="title"><?php echo get_the_title(); ?></h2>
+			<div class="s-flow-content">
+				<?php the_content(); ?>
+			</div>
+
+		</article><!-- #post -->
+	</div>
+	
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
