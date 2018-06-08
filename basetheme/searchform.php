369ceaa54ef1d019c2aa2ad329c325598f7301e3
Adam Taylor <AdamTaylor@core-marketing.co.uk>
2018-06-08 12:09:40 +0100

    Use text type instead of search type

--- basetheme/searchform.php
+++ basetheme/searchform.php
@@ -12,6 +12,6 @@ $unique_id = esc_attr( uniqid( 'search-form-' ) );
 	<label for="<?php echo $unique_id; ?>">
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'basetheme' ); ?></span>
 	</label>
-	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'basetheme' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+	<input type="text" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'basetheme' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	<button type="submit" class="search-submit"><span><?php echo _x( 'Search', 'submit button', 'basetheme' ); ?></span></button>
 </form>
